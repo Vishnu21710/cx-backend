2a1480f68c1cb091bc28d0bbec8953eaca5cc418
alphaware-vishnu <vishnu@alphawarenext.com>
2026-05-15 10:50:22 +0530

    fix:added cors

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
     origin: [
       process.env.FRONTEND_URL ?? 'http://localhost:5173',
       'http://localhost:5174',
+      'http://34.229.183.16:5173'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
